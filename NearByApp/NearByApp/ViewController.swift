4776355f4f00b66a1da41d6368b4f30c9deb7343
PCMacOS <mohamed.maher@eii-ahcc.com>
2020-07-10 16:10:34 +0200

    First Push

---
+++ NearByApp/NearByApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearByApp
+//
+//  Created by developer on 10/07/2020.
+//  Copyright © 2020 developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
